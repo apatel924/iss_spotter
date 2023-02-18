373fad2eaf011018402f21670bcf44ada5cd32be
apatel924 <apatel2@ualberta.ca>
2023-02-18 03:21:00 +0000

    commit iss fetcher and flyovertimes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes, nextISSTimesForMyLocation, } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -8,4 +8,38 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIp('68.145.1.17', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned coordinates: ', coords);
+});
+
+fetchISSFlyOverTimes({latitude: 51.041468, longitude: -114.055376}, (error, flyTimes) => {
+  if (error) {
+    console.log("It didn't work", error);
+    return;
+  }
+
+  console.log("It worked! Returned fly times:", flyTimes);
+});
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
--- iss.js
+++ iss.js
@@ -1,19 +1,85 @@
 const request = require('request');
 
 const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
-  request('https://api.ipify.org?format=json', (error, response, body){
+  request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
-      return callback (error, null);
+      return callback(error, null);
     }
 
     if (response.statusCode !== 200) {
-      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
       return;
     }
-    const ip = JSON.parse(body);
-    return callback(null, ip.ip);
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
 };
 
+const fetchCoordsByIp = function(ip, callback)  {
+  request(`https://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+  });
+};
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coordinates.latitude}&lon=${coordinates.longitude}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+  
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching fly over times. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+const nextISSTimesForMyLocation = function (callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIp(ip, (error, coordinates) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(coordinates, (error, passes) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, passes);
+      });
+    });
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
