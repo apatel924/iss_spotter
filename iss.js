d087e81e18d86ace6e5ec52af1b7020c16c5e15d
apatel924 <apatel2@ualberta.ca>
2023-02-18 01:58:06 +0000

    initial commit

---
+++ iss.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body){
+    if (error) {
+      return callback (error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const ip = JSON.parse(body);
+    return callback(null, ip.ip);
+  });
+};
+
